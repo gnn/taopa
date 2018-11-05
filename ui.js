4c8c1419ab05a19f5a9fbc2d68bdb58c3165f981
Stephan Günther <gnn.code@gmail.com>
2018-11-05 17:04:51 +0100

    Move all javascript code to external files

---
+++ ui.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var selected = "random";
+  $('#palette').change(function(e){
+    selected = $('#palette')[0].value;
+  });
+  $('#start').click(function(){ start(selected); });
+  $('#delay').change(function(){ delay = +$(this).val(); });
+  $('#stride').change(function(){ animate.stride = +$(this).val(); });
+  for (var palette of palettes.keys()){
+    $('#palette').append(
+        '<option ' +
+        (selected === palette ? "selected " : "") +
+        '"value="' + palette +'">' +
+        palette +
+        '</option>');
+  };
+});
+
