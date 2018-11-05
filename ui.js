e764562204ada37c8905b94d2076f31beb3bc7d1
Stephan Günther <gnn.code@gmail.com>
2018-11-06 00:13:13 +0100

    Adhere to Javascript style recommendations
    Which means putting a space between the `function` keyword and the
    parenthesis for the argument list for anonymous functions, as well as
    putting a space between the argument list and the opening curly brace
    for function declarations.

--- ui.js
+++ ui.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
   var selected = "random";
-  $('#palette').change(function(e){
+  $('#palette').change(function (e) {
     selected = $('#palette')[0].value;
   });
-  $('#start').click(function(){ start(selected); });
-  $('#delay').change(function(){ delay = +$(this).val(); });
-  $('#stride').change(function(){ animate.stride = +$(this).val(); });
+  $('#start').click(function () { start(selected); });
+  $('#delay').change(function () { delay = +$(this).val(); });
+  $('#stride').change(function () { animate.stride = +$(this).val(); });
   for (var palette of palettes.keys()){
     $('#palette').append(
         '<option ' +
